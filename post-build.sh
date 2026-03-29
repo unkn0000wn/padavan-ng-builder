ba38cfd61f8323800f551bb62bdf179d524b45fc
unkn0000wn <40450301+unkn0000wn@users.noreply.github.com>
2026-03-30 00:07:05 +0300

    Update post-build.sh

--- post-build.sh
+++ post-build.sh
@@ -1,1 +1,6 @@
 # script runs after the build
+FW_FILE_NAME="$(find padavan-ng/trunk/images -type f -regextype posix-extended -iregex ".*\.(trx|bin)$" \
+                        -printf "%T@\t%f\n" | sort -V | tail -1 | cut -f2)"
+cp "padavan-ng/trunk/images/$FW_FILE_NAME" .
+echo "FW_FILE_NAME=$FW_FILE_NAME" >> $GITHUB_ENV
+stat "padavan-ng/trunk/images/$FW_FILE_NAME"
