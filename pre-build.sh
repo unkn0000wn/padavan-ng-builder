9a22b48b7a7c8d4d99444c3437e60f18c8a65d06
unkn0000wn <40450301+unkn0000wn@users.noreply.github.com>
2026-03-29 21:30:29 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,3 +1,11 @@
 # script is run before the build
 # e.g.:
 # sudo apt install <pkg>
+
+echo "=== Pre-build start: $(date) ==="
+# 1. Timezone Москва
+ln -snf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+apt update
+DEBIAN_FRONTEND=noninteractive apt install tzdata -y
+export TZ=Europe/Moscow
+date  # Проверка: Thu Mar 26 14:27:00 MSK 2026
